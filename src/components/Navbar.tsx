2033ca54651326d85836409d194d5334fd69b2a7
Batuhan445 <neomatrix44@hotmail.com>
2023-08-28 16:17:17 +0300

    navbar fix and mobile fix started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,39 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
+
+  const [navbar, setNavbar] = useState(false)
+  
+
   return (
-    <div className='flex justify-between items-center m-auto h-16 px-10 shadow-lg bg-gray-500'>
+
+    <div className='flex justify-between items-center py-5 px-10 shadow-lg bg-gray-500 text-white'>
+
       <Link href={"/"}>Batuhan Karaahmetoğlu Blog</Link>
-      <div className='space-x-6'>
-        <Link href={"/"}>Ana Sayfa</Link>
-        <Link href={'/contact'}>İletişim</Link>
+
+      <div className='flex'>
+
+        <div className='md:hidden'>
+
+      <button>
+        <Image 
+        src={'/hamburger-menu.png'} 
+        alt={'hamburger-menu'} 
+        width={25} 
+        height={20}
+        />
+      </button>
+
+        </div>
+
+        <div className='hidden md:flex md:space-x-4 '>
+        <Link className='hover:text-gray-700 transition' href={"/"}>Ana Sayfa</Link>
+        <Link className='hover:text-gray-700 transition' href={'/contact'}>İletişim</Link>
+        </div>
+
       </div>
         
     </div>
