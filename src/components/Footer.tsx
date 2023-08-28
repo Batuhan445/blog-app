4bec2ca03de2c5ea617149e46cc5f0bb99d2f0d8
Batuhan445 <neomatrix44@hotmail.com>
2023-08-28 10:23:09 +0300

    social media added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,29 @@ import Link from 'next/link'
 export const Footer = () => {
   return (
     <div>
-      <div className="fixed bottom-0 h-20 md:h-16 grid md:flex md:justify-between items-center w-full md:px-10 bg-[#091f2c] text-center py-2 md:py-0">
+      <div className="fixed bottom-0 h-22 md:h-16 grid md:flex md:justify-between items-center w-full md:px-10 bg-[#091f2c] text-center py-2 md:py-0">
 
-        <div className='m-auto md:m-0'>
+        <div className='m-auto md:m-0 flex'>
+
+          <div>
+            <Link href="https://www.linkedin.com/in/batuhankaraahmeto%C4%9Flu/" target='_blank' className='inline-block p-2'>
+            <Image src={"/social-media/linkedin.png"} alt='linkedin-logo' width={24} height={24}/>
+            </Link>
+          </div>
+
+        <div>
+          <Link href="https://facebook.com/batuhankara.tr" target='_blank' className='inline-block p-2'>
+          <Image src={"/social-media/facebook.png"} alt='facebook-logo' width={24} height={24}/>
+          </Link>
+        </div>
+
+        <div className='mt-[2px]'>
 
           <Link href="https://x.com/trbatuhankara" target='_blank' className='inline-block p-2'>
           <Image 
           src={'/social-media/x-twitter-logo.png'} alt={'x-twitter-logo'} width={20} height={20}/>
           </Link>
+        </div>
 
         </div>
 
