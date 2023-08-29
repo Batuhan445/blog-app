94afd76f640c3349caa9df998b5cd2ae5ee162bd
Batuhan445 <neomatrix44@hotmail.com>
2023-08-29 13:08:00 +0300

    edited some social media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,25 +9,18 @@ export const Footer = () => {
 
         <div className='m-auto md:m-0 flex'>
 
+        <div>
+          <Link href="https://github.com/Batuhan445" target='_blank' className='inline-block p-2'>
+          <Image src={"/social-media/github.png"} alt='github-logo' width={24} height={24}/>
+          </Link>
+        </div>
+        
           <div>
             <Link href="https://www.linkedin.com/in/batuhankaraahmeto%C4%9Flu/" target='_blank' className='inline-block p-2'>
             <Image src={"/social-media/linkedin.png"} alt='linkedin-logo' width={24} height={24}/>
             </Link>
           </div>
 
-        <div>
-          <Link href="https://facebook.com/batuhankara.tr" target='_blank' className='inline-block p-2'>
-          <Image src={"/social-media/facebook.png"} alt='facebook-logo' width={24} height={24}/>
-          </Link>
-        </div>
-
-        <div className='mt-[2px]'>
-
-          <Link href="https://x.com/trbatuhankara" target='_blank' className='inline-block p-2'>
-          <Image 
-          src={'/social-media/x-twitter-logo.png'} alt={'x-twitter-logo'} width={20} height={20}/>
-          </Link>
-        </div>
 
         </div>
 
