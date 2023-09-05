7e92183ac9e6041ce674c109fdd9487ec6d0d4d7
Batuhan445 <neomatrix44@hotmail.com>
2023-09-05 15:07:07 +0300

    scroll top added and project added

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { CiShare1 } from 'react-icons/ci'
+import ScrollToTop from "react-scroll-to-top";
+
 
 const Projects = () => {
   return (
@@ -128,51 +131,50 @@ const Projects = () => {
             </div>
             
           </div>
-
-          
         </div>
 
-        {/* <div className="mt-10 lg:flex m-auto">
-          
-          <div className="m-auto relative w-[90%] h-48">
+        <div className="mt-16 grid m-auto">
+
+        <div className="m-auto">
             <Image
-              src={"/projects/epic-mix.jpg"}
+              src={"/projects/film-api.jpg"}
               style={{ borderRadius: "10px", objectFit:"contain" }}
-              alt={"universite-web-site"}
-              fill
+              alt={"film-api"}
+              width={720}
+              height={720}
+              className="w-[90%] m-auto"
             />
           </div>
 
-          <div className="mt-6 lg:m-auto">
-
-              <div className="ml-0 m-auto lg:ml-6">
-
-            <div className="text-center lg:text-left mt-4 lg:mt-0">
-                <h1 className="font-bold text-xl">Epic Mix</h1>
-                <p className="w-[80%] m-auto lg:w-auto">
-                  Javascript olarak kullandığım ilk projemdir. Show Me! butonuna tıklandığında
-                  şarkıların listesi gözükmektedir. Şarkı ekleme konusunda sadece YouTube linki şartı koşulmuştur.
-                  YouTube dışında bir link olursa uyarı vermektedir. Silmek isterseniz uyarı çıkıyor ve 
-                  sildikten sonra listenin sayısı güncelleniyor.
-                </p>
+          <div className="m-auto">
+            <div className="m-auto text-center">
+              <h1 className="font-bold text-xl mt-6">Film Sitesi</h1>
+              <p className="w-[80%] m-auto">
+                Bu projede API kullanarak veri çekildi ve filmlerin bilgileri
+                görüntülenmesini sağladım. Ayrıca Karanlık modu desteği vardır
+              </p>
             </div>
-              
 
-              <div className="mx-auto lg:mx-0 mt-8 w-28">
-                <Link
-                  href={"https://willowy-belekoy-d5e574.netlify.app/"}
-                  target="_blank"
-                  className="border p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
-                >
-                  Ziyaret Edin
-                </Link>
-              </div>
-
+            <div className="mx-auto w-36 my-8 rounded">
+              <Link
+                href={"https://frolicking-kashata-5731c1.netlify.app/"}
+                target="_blank"
+                className="border flex justify-center items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
+              >
+                Ziyaret Edin
+                <div className="ml-2">
+                <CiShare1/>
+                </div>
+              </Link>
             </div>
+            
           </div>
+        </div>
 
 
-        </div> */}
+        <ScrollToTop
+        className="flex justify-center items-center transition"
+        smooth/>
 
       
 
