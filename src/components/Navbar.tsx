f9d8daf77705cb29c02a0df07693055fe2b628de
Batuhan445 <neomatrix44@hotmail.com>
2023-09-17 20:26:19 +0300

    more changed. still editing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,97 +1,159 @@
-"use client"
-import Link from 'next/link'
-import React, { useState } from 'react'
-import Image from 'next/image'
-import { FaTimes } from 'react-icons/fa'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaTimes } from "react-icons/fa";
+import { Merriweather_Sans } from "next/font/google";
+import { useRouter } from 'next/navigation'
+
+const merriweatherSans = Merriweather_Sans({ subsets: ["latin"] });
 
 const Navbar = () => {
 
-  let Links = [
-    {name: "Ana Sayfa", to: "/"},
-    {name: "Projelerim", to:"/my-projects"},
-    {name: "İletişim", to:"/contact"},
-  ]
+  const router = useRouter()
+  const [activeLink, setActiveLink] = useState("");
 
-  const [navbarOpen, setNavbarOpen] = useState(false)
-  
+  // Navbar Links
+
+  let Links = [
+    { name: "Ana Sayfa", to: "/", Image: "/navbar-icons/home.png" },
+    {
+      name: "Projelerim",
+      to: "/my-projects",
+      Image: "/navbar-icons/projects.png",
+    },
+    { name: "İletişim", to: "/contact", Image: "/navbar-icons/contact.png" },
+  ];
+
+  // Mobile Navbar
+
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   const toggleNavbar = () => {
-    setNavbarOpen(!navbarOpen)
-  }
+    setNavbarOpen(!navbarOpen);
+  };
 
   const closeNavbar = () => {
     setNavbarOpen(false);
   };
 
-  
+  // Page
 
   return (
+    <div className="flex justify-between items-center py-6 px-10 shadow-lg bg-[#091f2c] text-white">
 
-    <div className='flex justify-between items-center py-5 px-10 shadow-lg bg-gray-500 text-white'>
+      <div className={`${merriweatherSans.className} text-xl md:text-3xl`}>
+        <Link href={"/"}>Batuhan Karaahmetoğlu</Link>
+      </div>
 
-      <Link href={"/"}>Batuhan Karaahmetoğlu Blog</Link>
+      <div className="flex">
 
-      <div className='flex'>
-
-        <div className='md:hidden'>
-
-      <button onClick={toggleNavbar}>
-        <Image 
-        src={'/hamburger-menu.png'} 
-        alt={'hamburger-menu'} 
-        width={25} 
-        height={20}
-        style={{marginTop:"4px"}}
-        />
-      </button>
+        <div className="md:hidden">
+          <button onClick={toggleNavbar}>
+            <Image
+              src={"/hamburger-menu.png"}
+              alt={"hamburger-menu"}
+              width={30}
+              height={20}
+              style={{ marginTop: "4px" }}
+            />
+          </button>
 
         </div>
 
+        {/* Mobile Navbar Open Background */}
 
         <div className="md:hidden">
-            {navbarOpen && (
-              <div
-                className="fixed  top-0 left-0 w-full h-full transform duration-1000 transition-transform bg-black opacity-50"
-                onClick={closeNavbar}
-              ></div>
-            )}
-
+          {navbarOpen && (
             <div
-              className={`fixed z-10 top-0 right-0 h-screen w-2/4 bg-gray-500 shadow-lg shadow-zinc-600 text-white p-6 transform duration-1000 transition-transform ${
-                navbarOpen ? "translate-x-0" : "translate-x-full"
-              }`}
-            >
-              <div className="flex justify-center flex-col">
-                <button
-                  onClick={closeNavbar}
-                  className="ml-auto my-auto p-2 text-white text-2xl"
-                >
-                  <FaTimes />
-                </button>
-                {Links.map((link) => (
-                  <div className="mr-auto mt-2" key={link.to}>
-                    <Link href={link.to} onClick={closeNavbar}>
-                      {link.name}
-                    </Link>
-                  </div>
-                ))}
-              </div>
+              className="fixed top-0 left-0 w-full h-full transform duration-1000 transition-transform bg-black opacity-50"
+              onClick={closeNavbar}
+            ></div>
+          )}
+
+          <div
+            className={`fixed z-10 top-0 right-0 h-screen w-2/4 bg-[#091f2c] shadow-zinc-600 text-white p-6 transform duration-700 transition-transform ${
+              navbarOpen ? "translate-x-0 shadow-lg" : "translate-x-full"
+            }`}
+          >
+
+            <div className="flex justify-center flex-col">
+
+              <button
+                onClick={closeNavbar}
+                className="ml-auto my-auto p-2 text-white text-2xl"
+              >
+                <FaTimes />
+              </button>
+
+              {Links.map((link) => (
+                <div className="mr-auto mt-6" key={link.to}>
+
+                  <Link href={link.to} onClick={closeNavbar}>
+
+                    <div className="flex">
+
+                      <div className="mr-3 flex items-center">
+
+                        <Image
+                          src={link.Image}
+                          alt={link.name}
+                          width={23}
+                          height={20}
+                        />
+
+                      </div>
+
+                      <div className={merriweatherSans.className}>
+
+                        {link.name}
+
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ))}
             </div>
           </div>
+        </div>
 
 
-        <div className='hidden md:flex md:space-x-4'>
-          {Links.map(link => (
-            <Link key={link.to} href={link.to} className='hover:text-gray-300 transition'>
-              {link.name}
+      {/* Desktop Navbar Links */}
+
+        <div className="hidden md:flex md:space-x-10">
+
+          {Links.map((link) => (
+            <Link
+              key={link.to}
+              href={link.to}
+              className={`hover:text-white transition ${
+                activeLink === link.name ? "opacity-100" : "opacity-50"
+              }`}
+              onClick={() => setActiveLink(link.name)}
+            >
+
+              <div className="flex">
+
+                <div className="mr-2 flex items-center">
+                  <Image
+                    src={link.Image}
+                    alt={link.name}
+                    width={23}
+                    height={20}
+                  />
+                </div>
+
+                <div className={merriweatherSans.className}>{link.name}</div>
+              </div>
+
             </Link>
           ))}
         </div>
 
       </div>
-        
+
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
