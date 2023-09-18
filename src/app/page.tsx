bbb3588bce60c8e528f0ee8342915101b9ffbd39
Batuhan445 <neomatrix44@hotmail.com>
2023-09-18 12:08:25 +0300

    added font and changed image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,38 @@
 import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link'
+import { Work_Sans } from 'next/font/google'
+
+const workSans = Work_Sans({subsets: ["latin"]})
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen bg-white text-black'>
-      <div className='text-center text-4xl my-10'>
+    <div className={`min-h-screen bg-white text-black ${workSans.className}`}>
+
+      <div 
+      className={"text-center text-5xl my-10"}
+      >
       Blog Sayfama Hoşgeldiniz
       </div>
 
-      <div className='m-auto w-auto'>
-        <Image 
-        src={"/portfoilo.jpg"} 
-        style={{ borderRadius: "10px", objectFit: "contain"}} 
-        alt={"portfoilo"} 
-        width={720}
-        height={720}
+      
+      <div className='m-auto w-[25rem] md:w-[30rem]'>
+        <img 
+        src="/software.jpg" 
+        style={{ borderRadius: "20px", objectFit: "contain", width: "100%", height: "100%" }} 
+        alt="software" 
         className="w-[50%] h-48 lg:h-96 m-auto"
         />
       </div>
+      
 
       <div className='mt-8 w-[80%] text-center m-auto'>
         <h1 className='font-bold text-2xl'>Hakkımda</h1>
-        <p>Frontend Öğreniyorum. 2021 yılında Clarusway üzerinden frontend kursu aldım.
-          Hobi olarak ise oyun oynarım
-        </p>
+        <p>Herkese Merhaba. Ben Batuhan Karaahmetoğlu. Frontend Developer olma yolunda gidiyorum.</p>
+        <p className='my-4 md:my-0'>Amerika merkezli Clarusway şirketinden 2020 - 2021 yılında kursu alarak olarak süreci ilerlettim.</p>
+        <p>Şu anda ise Frontend üzerinde projeler yaparak kendimi ilerlettiriyorum</p>
 
         <h1 className='font-bold text-2xl mt-8'>Kullandığım Teklonojiler</h1>
-        <ol className='mt-2'>
+        <ol className='mt-2 w-32 text-left m-auto'>
           <li>- HTML</li>
           <li>- CSS</li>
           <li>- JavaScript</li>
@@ -38,7 +44,14 @@ const HomePage = () => {
 
     <div className='mb-10'>
         <h1 className='font-bold text-2xl mt-8'>Sertifikalarım</h1>
-        <p>Lise : (2014 - 2018)</p>
+      <div className='grid items-center text-left w-28 m-auto mt-3'>
+        <Link href={"https://c11n.clarusway.com/en/verify/73665382210162"}>- HTML & CSS</Link>
+        <Link href={"https://c11n.clarusway.com/en/verify/99307460379857"}>- JavaScript</Link>
+        <Link href={"https://c11n.clarusway.com/en/verify/90672329477884"}>- React</Link>
+        <Link href={"https://c11n.clarusway.com/en/verify/76802605708716"}>- Backend</Link>
+        <Link href={"https://c11n.clarusway.com/en/verify/41024071505791"}>- FullStack</Link>
+      </div>
+
     </div>
       </div>
 
