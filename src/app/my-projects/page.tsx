06832a0fae27b2261757055c8a63223b8209e80a
Batuhan445 <neomatrix44@hotmail.com>
2023-09-18 17:30:34 +0300

    changed projects info

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -4,11 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { CiShare1 } from 'react-icons/ci'
 import ScrollToTop from "react-scroll-to-top";
+import { Work_Sans } from 'next/font/google'
+
+const workSans = Work_Sans({subsets: ["latin"]})
 
 
 const Projects = () => {
   return (
-    <div className="min-h-screen">
+    <div className={`min-h-screen ${workSans.className}`}>
       <title>Batuhan Karaahmetoğlu Blog | Projelerim</title>
 
       <div className="my-10 text-3xl text-center">Projelerim</div>
@@ -19,7 +22,7 @@ const Projects = () => {
           
             <Image
               src={"/projects/university-web.jpg"}
-              style={{ objectFit:"contain", borderRadius: "10px" }}
+              style={{ objectFit:"contain", borderRadius: "10px", boxShadow:"1px 3px 15px black" }}
               alt={"universite-web-site"}
               width={720}
               height={720}
@@ -31,17 +34,18 @@ const Projects = () => {
           <div className="m-auto">
             <div className="text-center mt-4">
               <h1 className="text-xl font-bold">Üniversite Web Sitesi</h1>
-              <p className="w-[80%] lg:w-full m-auto">
-                Bu sitede HTML ve CSS kullandım. Butona tıklayarak scroll ve fotoğrafın üzerine getirdiğinizde hover özelliği ile ülkelerin
-                isimleri yazıyor.
+              <p className="w-[80%] m-auto">
+              Bu web sitesinde HTML ve CSS kullanarak çalıştım. Sayfa üzerindeki "Enroll Now" 
+              butonuna tıkladığınızda, sayfa aşağı kayarak kampüsleri görüntüler ve kampüslerin 
+              üzerine geldiğinizde ülkelerin isimlerini gösteren bir hover efekti ekledik.
               </p>
             </div>
 
-            <div className="m-auto w-36 mt-8  rounded ">
+            <div className="m-auto w-36 mt-8 rounded">
               <Link
                 href={"https://calm-gumdrop-f04589.netlify.app/"}
                 target="_blank"
-                className="flex justify-center border items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
+                className="flex justify-center border border-black hover:text-white items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
               >
                 Ziyaret Edin
                 <div className="ml-2">
@@ -59,7 +63,7 @@ const Projects = () => {
           
             <Image
               src={"/projects/epic-mix.jpg"}
-              style={{ borderRadius: "10px", objectFit:"contain" }}
+              style={{ borderRadius: "10px", objectFit:"contain", boxShadow:"1px 3px 15px black" }}
               alt={"universite-web-site"}
               width={720}
               height={720}
@@ -72,10 +76,9 @@ const Projects = () => {
             <div className="text-center mt-4">
               <h1 className="text-xl font-bold">Epic Mix</h1>
               <p className="w-[80%] m-auto">
-              Javascript olarak kullandığım ilk projemdir. Show Me! butonuna tıklandığında
-                  şarkıların listesi gözükmektedir. Şarkı ekleme konusunda sadece YouTube linki şartı koşulmuştur.
-                  YouTube dışında bir link olursa uyarı vermektedir. Silmek isterseniz uyarı çıkıyor ve 
-                  sildikten sonra listenin sayısı güncelleniyor.
+              İlk JavaScript projesimdir. Bir düğmeye tıkladığınızda, şarkıların bir listesi görüntülenir. 
+              Yalnızca YouTube bağlantıları eklemeye izin verilir, başka bir tür bağlantı eklerseniz uyarı alırsınız. 
+              Şarkıları silme seçeneği sunulur ve silindiğinde liste otomatik olarak güncellenir.
               </p>
             </div>
 
@@ -83,7 +86,7 @@ const Projects = () => {
             <Link
                 href={"https://willowy-belekoy-d5e574.netlify.app/"}
                 target="_blank"
-                className="flex justify-center border items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
+                className="flex justify-center border border-black hover:text-white items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
               >
                 Ziyaret Edin
                 <div className="ml-2">
@@ -100,7 +103,7 @@ const Projects = () => {
         <div className="m-auto">
             <Image
               src={"/projects/prime-properties.jpg"}
-              style={{ borderRadius: "10px", objectFit:"contain" }}
+              style={{ borderRadius: "10px", objectFit:"contain", boxShadow:"1px 3px 15px black" }}
               alt={"universite-web-site"}
               width={720}
               height={720}
@@ -112,8 +115,8 @@ const Projects = () => {
             <div className="m-auto text-center">
               <h1 className="font-bold text-xl mt-6">Prime Properties</h1>
               <p className="w-[80%] m-auto">
-                Bu projede ilk kez React kullandım ve Tailwind CSSte içeriyor. Sadece input bölümünde Javascript
-                kullanılmıştır.
+              Bu projede React'i ilk defa kullandım ve içinde Tailwind CSS kullanıldı. 
+              Sadece iletişim'in mesaj bölümünde JavaScript kullanıldı.
               </p>
             </div>
 
@@ -121,7 +124,7 @@ const Projects = () => {
               <Link
                 href={"https://mellifluous-profiterole-ba5e3a.netlify.app/"}
                 target="_blank"
-                className="border flex justify-center items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
+                className="border border-black hover:text-white flex justify-center items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
               >
                 Ziyaret Edin
                 <div className="ml-2">
@@ -138,7 +141,7 @@ const Projects = () => {
         <div className="m-auto">
             <Image
               src={"/projects/film-api.jpg"}
-              style={{ borderRadius: "10px", objectFit:"contain" }}
+              style={{ borderRadius: "10px", objectFit:"contain", boxShadow:"1px 3px 15px black" }}
               alt={"film-api"}
               width={720}
               height={720}
@@ -150,8 +153,9 @@ const Projects = () => {
             <div className="m-auto text-center">
               <h1 className="font-bold text-xl mt-6">Film Sitesi</h1>
               <p className="w-[80%] m-auto">
-                Bu projede API kullanarak veri çekildi ve filmlerin bilgileri
-                görüntülenmesini sağladım. Ayrıca Karanlık modu desteği vardır
+              Bu projede API kullanarak film verilerini çekip, 
+              bu filmlerin bilgilerinin görüntülenmesini sağladım. 
+              Ayrıca, karanlık mod desteği de ekledik.
               </p>
             </div>
 
@@ -159,7 +163,7 @@ const Projects = () => {
               <Link
                 href={"https://frolicking-kashata-5731c1.netlify.app/"}
                 target="_blank"
-                className="border flex justify-center items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
+                className="border border-black hover:text-white flex justify-center items-center p-3 rounded transition text-center hover:bg-blue-500 hover:border-blue-500"
               >
                 Ziyaret Edin
                 <div className="ml-2">
@@ -173,8 +177,8 @@ const Projects = () => {
 
 
         <ScrollToTop
-        className="flex justify-center items-center transition"
-        style={{boxShadow:"none", bottom:"50px"}}
+        className="flex justify-center items-center transition text-black"
+        style={{bottom:"50px", boxShadow:"1px 1px 15px black"}}
         smooth/>
 
       
